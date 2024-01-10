6904e2ce71069fab3f80d1c60169c5c8ed33c47b
gohelkrupali <155124047+gohelkrupali@users.noreply.github.com>
2024-01-10 11:15:30 +0530

    Add files via upload

---
+++ tablehtml.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+<form method='get' action="table1.php">
+enter number for start:<input type="text" name="sno"><br>
+enter number for start:<input type="text" name="lno"><br>
+<input type="submit" name="submit">
+</form>
+</body>
+</head>
+</html>
